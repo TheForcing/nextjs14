3e9309cbfeda81b97c6fd5d3db26dc2c0a5f8982
박지훈 <qkrw6@naver.com>
2024-03-18 16:51:50 +0900

    api url dev

--- components/movie-videos.tsx
+++ components/movie-videos.tsx
@@ -1,4 +1,5 @@
-import { API_URL } from "../app/(home)/page";
+
+import { API_URL } from "../app/constants";
 import styles from "../app/styles/movie-video.module.css"
 
 async function getVideos(id: string) {
